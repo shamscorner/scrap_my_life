cc348fb499192196617656968bd244afcfb64afb
Shamim Hossain <hossains159@gmail.com>
2020-12-17 17:06:00 +0600

    feat: add different pattern background

--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,11 @@
 <template>
   <div>
-    <div class="grid">
-      <header>
-        <h1>Hector’s Adventures with George</h1>
-      </header>
-      <figure class="fig fig--1" style="--aspect-ratio: 3/4">
-        <img
-          src="https://images.unsplash.com/photo-1515488042361-ee00e0ddd4e4?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
-          alt="Small child playing with toys"
-        />
-      </figure>
-      <p>
-        We played in Nana and Grandad’s garden. We picked apples and made apple
-        crumble!
-      </p>
-      <figure class="fig fig--2" style="--aspect-ratio: 4/3">
-        <img
-          src="https://images.unsplash.com/photo-1502086223501-7ea6ecd79368?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
-        />
-      </figure>
-      <p>We had a game of football.</p>
-      <figure class="fig fig--3" style="--aspect-ratio: 3/4">
-        <img
-          src="https://images.unsplash.com/photo-1472162072942-cd5147eb3902?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
-          alt="Child reading"
-        />
-      </figure>
-      <p>We read a funny story together.</p>
-    </div>
+    <input type="file" name="moments[]" multiple />
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  name: 'Home',
+}
 </script>
-
-<style></style>
---
+++ pages/scrapbook.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="scrapbook">
+    <div v-if="!isCanvas" class="grid">
+      <header>
+        <h1>I Love My Awesome + Cute Cat</h1>
+      </header>
+      <figure class="fig fig--1">
+        <img
+          src="~/assets/images/image1.jpg"
+          alt="Small child playing with toys"
+        />
+      </figure>
+      <p>We played in my parent's house and they love him so much.</p>
+      <figure class="fig fig--2">
+        <img src="~/assets/images/image2.jpg" />
+      </figure>
+      <p>We had a great walk in park. Obviously, he had to prepare.</p>
+      <figure class="fig fig--3">
+        <img src="~/assets/images/image3.jpg" />
+      </figure>
+      <p>We watched a horror movie together.</p>
+    </div>
+    <div id="canvasContainer"></div>
+  </div>
+</template>
+
+<script>
+import html2canvas from 'html2canvas'
+
+export default {
+  name: 'Scrapbook',
+
+  data() {
+    return {
+      isCanvas: false,
+    }
+  },
+
+  async mounted() {
+    try {
+      const generatedCanvas = await html2canvas(
+        document.querySelector('#scrapbook')
+      )
+
+      // show the canvas
+      this.isCanvas = true
+
+      // put the canvas
+      document.querySelector('#canvasContainer').appendChild(generatedCanvas)
+    } catch (error) {
+      console.log(error)
+    }
+  },
+}
+</script>
